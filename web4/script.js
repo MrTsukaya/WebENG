9f8b14c1a1a37946fcab2a9e7e90ed073ecc63e5
MrTsukaya <57458434+MrTsukaya@users.noreply.github.com>
2021-04-08 01:23:15 +0200

    Added notRead and active tag, auto content loading

--- web4/script.js
+++ web4/script.js
@@ -1,20 +1,38 @@
-let unreadMessages = 0;
+let unreadMessages = document.getElementsByClassName('notReadMail').length;
 let unreadCounter = document.getElementById("counter");
+let mailContent = document.getElementById("contentBig");
 
 function check(x){
-    let ifTrue = x.classList.contains('selectedMail');
-    if(ifTrue){
-        x.className = "message";
+    let ifNotRead = x.classList.contains('notReadMail');
+    let ifActive = x.classList.contains('activeMail');
+    let howMany = document.getElementsByClassName('message');
+
+    if(ifNotRead){
+        x.classList.remove('notReadMail');
         unreadMessages--;
     }
-    else{
-        x.className = "message selectedMail";
+    /*else{
+        x.className = "message notReaddMail";
         unreadMessages++;
+    }*/
+    if(!ifActive){
+        for(let i=0;i<howMany.length;i++)
+            howMany[i].classList.remove("activeMail");
+        x.classList.add("activeMail");
+        let temp = 0;
+
+        for(let j=0;j<emails.length;j++)
+            if(emails[j].title == x.children[1].children[1].innerHTML)
+                temp = j;
+        
+        console.log(temp);
+        mailContent.innerHTML = emails[temp].content;
     }
     unreadCounter.innerHTML = "("+unreadMessages+")";
 }
 
 window.onload = ()=> {
+    unreadCounter.innerHTML = "("+unreadMessages+")";
     let x = document.getElementsByClassName('mailName');
     let y = document.getElementsByClassName('mailTitle');
     let z = document.getElementsByClassName('mailCont');
