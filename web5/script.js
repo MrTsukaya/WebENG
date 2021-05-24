661be0263292b7e68d7422b015f74839cc0893a9
MrTsukaya <57458434+MrTsukaya@users.noreply.github.com>
2021-05-24 13:10:42 +0200

    Added card animations, adjusted keyframes for each

--- web5/script.js
+++ web5/script.js
@@ -1,13 +1,23 @@
 var isLeftActive = false;
 var button = document.getElementById("switchButton");
+var leftCard = document.getElementById("leftCard");
+var rightCard = document.getElementById("rightCard");
 
 function Swap() {
     if(isLeftActive){
         button.innerHTML = '<i class="fas fa-arrow-alt-circle-left" style="color:white;"></i> Switch to Happy Sushi Lunch';
         isLeftActive = false;
+        rightCard.classList.remove("rightInactive");
+        leftCard.classList.remove("leftActive");
+        rightCard.classList.add("rightActive");
+        leftCard.classList.add("leftInactive");
     }
     else{
         button.innerHTML = 'Switch to Sushi Lover\'s Lunch! <i class="fas fa-arrow-alt-circle-right" style="color:white;"></i>';
         isLeftActive = true;
+        rightCard.classList.remove("rightActive");
+        leftCard.classList.remove("leftInactive");
+        rightCard.classList.add("rightInactive");
+        leftCard.classList.add("leftActive");
     }
 }
