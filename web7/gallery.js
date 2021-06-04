3974b7104beee3142ebf620e775506fdad0a98f9
MrTsukaya <57458434+MrTsukaya@users.noreply.github.com>
2021-06-04 14:20:46 +0200

    Minor changes, done blog.html

--- web7/gallery.js
+++ web7/gallery.js
@@ -1,73 +1,77 @@
+// Skrypt pilnuje aktywnego zdjęcia, a slider działa jedynie w obszarze aktywnej galerii, tj. wczytuje tylko zdjęcia,
+// które w niej się znajdują
+// Pętla slidera jest nieskończona
+
 var modal = document.getElementById("modal");
-           var modalContent = document.getElementById("modalContent");
-           
-           var images1 = document.getElementsByClassName("gallery1");
-           var images2 = document.getElementsByClassName("gallery2");
-           var images3 = document.getElementsByClassName("gallery3");
+var modalContent = document.getElementById("modalContent");
 
-           var close = document.getElementById("closeX");
-           var previous = document.getElementById("previous");
-           var next = document.getElementById("next");
-           var now = {
-            nowGallery: null,
-            nowPicture: 0
-           };
+var images1 = document.getElementsByClassName("gallery1");
+var images2 = document.getElementsByClassName("gallery2");
+var images3 = document.getElementsByClassName("gallery3");
 
-            for(let i = 0; i<images1.length; i++){
-                images1[i].addEventListener("click", () => {
-                now.nowGallery = images1;
-                now.nowPicture = i;
-                show(images1[i].src);
-                });
-            }
-            for(let i = 0; i<images2.length; i++){
-                images2[i].addEventListener("click", () => {
-                now.nowGallery = images2;
-                now.nowPicture = i;
-                show(images2[i].src);
-                });
-            }
-            for(let i = 0; i<images3.length; i++){
-                images3[i].addEventListener("click", () => {
-                now.nowGallery = images3;
-                now.nowPicture = i;
-                show(images3[i].src);
-                });
-            }
+var close = document.getElementById("closeX");
+var previous = document.getElementById("previous");
+var next = document.getElementById("next");
+var now = {
+nowGallery: null,
+nowPicture: 0
+};
 
-            previous.addEventListener("click", ()=>{
-                slide(now.nowGallery,now.nowPicture,"left");
-            });
-            next.addEventListener("click", ()=>{
-                slide(now.nowGallery,now.nowPicture,"right");
-            });
+for(let i = 0; i<images1.length; i++){
+    images1[i].addEventListener("click", () => {
+    now.nowGallery = images1;
+    now.nowPicture = i;
+    show(images1[i].src);
+    });
+}
+for(let i = 0; i<images2.length; i++){
+    images2[i].addEventListener("click", () => {
+    now.nowGallery = images2;
+    now.nowPicture = i;
+    show(images2[i].src);
+    });
+}
+for(let i = 0; i<images3.length; i++){
+    images3[i].addEventListener("click", () => {
+    now.nowGallery = images3;
+    now.nowPicture = i;
+    show(images3[i].src);
+    });
+}
 
-            closeX.addEventListener("click", ()=>{
-                modal.style.visibility = 'hidden';
-            })
-            modalContent.addEventListener("click", ()=> {
-                console.log("działa");
-            })
+previous.addEventListener("click", ()=>{
+    slide(now.nowGallery,now.nowPicture,"left");
+});
+next.addEventListener("click", ()=>{
+    slide(now.nowGallery,now.nowPicture,"right");
+});
 
-            function show(link){
-                modal.style.visibility = 'visible';
-                modalContent.style.backgroundImage = "url("+link+")";
-            }
-            function slide(gallery,picture,direction){
-                if(direction == "right"){
-                    if(gallery[picture+1] == null){
-                        picture = -1;
-                        now.nowPicture = -1;    
-                    }
-                    now.nowPicture += 1;
-                    show(gallery[picture+1].src);
-                }
-                if(direction == "left"){
-                    if(picture == 0){
-                        picture = gallery.length;
-                        now.nowPicture = gallery.length;
-                    }
-                    now.nowPicture -= 1;
-                    show(gallery[picture-1].src);
-                }
-            }
+closeX.addEventListener("click", ()=>{
+    modal.style.visibility = 'hidden';
+})
+modalContent.addEventListener("click", ()=> {
+    console.log("działa");
+})
+
+function show(link){
+    modal.style.visibility = 'visible';
+    modalContent.style.backgroundImage = "url("+link+")";
+}
+function slide(gallery,picture,direction){
+    if(direction == "right"){
+        if(gallery[picture+1] == null){
+            picture = -1;
+            now.nowPicture = -1;    
+        }
+        now.nowPicture += 1;
+        show(gallery[picture+1].src);
+    }
+    if(direction == "left"){
+        if(picture == 0){
+            picture = gallery.length;
+            now.nowPicture = gallery.length;
+        }
+        now.nowPicture -= 1;
+        show(gallery[picture-1].src);
+    }
+}
